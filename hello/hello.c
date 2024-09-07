bde36ce5967d2247fa79e01b0660a195d132c56e
seven1oe <seven1oe@users.noreply.github.com>
2024-09-07 23:07:35 +0800

    test

--- hello/hello.c
+++ hello/hello.c
@@ -1,16 +1,19 @@
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+// Module metadata
+MODULE_AUTHOR("Ruan de Bruyn");
+MODULE_DESCRIPTION("Hello world driver");
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Robert W. Oliver II");
-MODULE_DESCRIPTION("A simple example Linux module.");
-MODULE_VERSION("0.01");
-static int __init lkm_example_init(void) {
- printk(KERN_INFO "Hello, World!\n");
+// Custom init and exit methods
+static int __init custom_init(void) {
+ printk(KERN_INFO "Hello world driver loaded.");
  return 0;
 }
-static void __exit lkm_example_exit(void) {
- printk(KERN_INFO "Goodbye, World!\n");
+static void __exit custom_exit(void) {
+ printk(KERN_INFO "Goodbye my friend, I shall miss you dearly...");
 }
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
+module_init(custom_init);
+module_exit(custom_exit);
