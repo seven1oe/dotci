199057c7eca8335a0c14bb51e07655875485a041
seven1oe <seven1oe@users.noreply.github.com>
2024-09-08 00:40:52 +0800

    test

--- hello/hello.c
+++ hello/hello.c
@@ -1,69 +1,24 @@
+#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
-#include <asm/uaccess.h>
-#include <linux/fs.h> 
-#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
 #include <linux/proc_fs.h>
+#include <linux/sched.h>
 #include <linux/pid.h>
+// Module metadata
+MODULE_AUTHOR("Ruan de Bruyn");
+MODULE_DESCRIPTION("Hello world driver");
+MODULE_LICENSE("GPL");
+// Custom init and exit methods
+static int __init custom_init(void) {
+ printk(KERN_INFO "Hello world driver loaded.");
+ struct task_struct *my_task;
+my_task = pid_task(find_vpid(652), PIDTYPE_PID);
 
-#include <linux/pid_namespace.h>
-int p_id;
-struct pid *pid_struct;
-struct task_struct *task;
-static struct proc_dir_entry *proc_write_entry;
-char *proc_name="task_by_pid";
-
-int read_proc(char *buf,char **start,off_t offset,int count,int *eof,void *data )
-{
-int len=0;
-pid_struct = find_get_pid(p_id);
-task = pid_task(pid_struct,PIDTYPE_PID);
-
- len = sprintf(buf,"\nname %s\n ",task->comm);
-
-return len;
+ return 0;
 }
-
-int write_proc(struct file *file,const char *buf,int count,void *data )
-{
-int ret;
-char *id;
-id = (char *)kmalloc(1000*sizeof(char),GFP_KERNEL);
-printk(KERN_INFO "buf passed %s",buf);
-if(copy_from_user(id,buf,count))
-    return -EFAULT;
-printk(KERN_INFO "id passed %s",id);
-p_id = simple_strtoul(id,NULL,0);
-printk(KERN_INFO "pid %d ret %d",p_id,ret);
-return sizeof(buf);
+static void __exit custom_exit(void) {
+ printk(KERN_INFO "Goodbye my friend, I shall miss you dearly...");
 }
-
-void create_new_proc_entry()
-{
-proc_write_entry = create_proc_entry(proc_name,0666,NULL);
-if(!proc_write_entry)
-      {
-    printk(KERN_INFO "Error creating proc entry");
-    return -ENOMEM;
-    }
-proc_write_entry->read_proc = read_proc ;
-proc_write_entry->write_proc = write_proc;
-printk(KERN_INFO "proc initialized");
-
-}
-
-
-
-int proc_init (void) {
-    create_new_proc_entry();
-    return 0;
-}
-
-void proc_cleanup(void) {
-    printk(KERN_INFO " Inside cleanup_module\n");
-    remove_proc_entry(proc_name,NULL);
-}
-MODULE_LICENSE("GPL");   
-module_init(proc_init);
-module_exit(proc_cleanup);
-
+module_init(custom_init);
+module_exit(custom_exit);
