bcf79dab92e26c37716e33f58f736f7c3a3398d6
seven1oe <seven1oe@users.noreply.github.com>
2024-09-08 00:39:34 +0800

    test

--- hello/hello.c
+++ hello/hello.c
@@ -1,56 +1,69 @@
-#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/pci.h>
-#include <linux/slab.h>
-#include <linux/interrupt.h>
-#include <linux/dma-mapping.h>
-#include <sound/initval.h>
-#include <linux/delay.h>
-#include <linux/kthread.h>
 #include <linux/kernel.h>
-#include <asm/signal.h>
-#include <linux/delay.h>
-#include <linux/sched.h>
-#include <linux/sched/signal.h>
-static int my_init(void)
-{
-	struct task_struct *task;
-	int signum = SIGKILL;
-	struct kernel_siginfo info;
-	memset(&info, 0, sizeof(struct kernel_siginfo));
-	info.si_signo = signum;
-	while(1)
-	{
-		for_each_process(task)
-		{
-			/* find a sleep process */
-			if (strncmp(task->comm, "sleep", 5) == 0)
-			{	
-				int ret = send_sig_info(signum, &info, task);
-				if (ret < 0)
-				{
-				  printk(KERN_INFO "error sending signal\n");
-				}
-				else 
-				{
-					printk(KERN_INFO "Target has been killed\n");
-				}
-			}
-		}
-	}
-	return 0;
-}
-   
-static void my_exit(void)
-{
-    printk(KERN_INFO "Exit driver.\n");
+#include <asm/uaccess.h>
+#include <linux/fs.h> 
+#include <linux/cdev.h>
+#include <linux/proc_fs.h>
+#include <linux/pid.h>
 
-    return;
-}
-   
-module_init(my_init);
-module_exit(my_exit);
+#include <linux/pid_namespace.h>
+int p_id;
+struct pid *pid_struct;
+struct task_struct *task;
+static struct proc_dir_entry *proc_write_entry;
+char *proc_name="task_by_pid";
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Shubham Dubey <shubham0d@protonmail.coms>");
-MODULE_DESCRIPTION("Kill process from kernel module");
+int read_proc(char *buf,char **start,off_t offset,int count,int *eof,void *data )
+{
+int len=0;
+pid_struct = find_get_pid(p_id);
+task = pid_task(pid_struct,PIDTYPE_PID);
+
+ len = sprintf(buf,"\nname %s\n ",task->comm);
+
+return len;
+}
+
+int write_proc(struct file *file,const char *buf,int count,void *data )
+{
+int ret;
+char *id;
+id = (char *)kmalloc(1000*sizeof(char),GFP_KERNEL);
+printk(KERN_INFO "buf passed %s",buf);
+if(copy_from_user(id,buf,count))
+    return -EFAULT;
+printk(KERN_INFO "id passed %s",id);
+p_id = simple_strtoul(id,NULL,0);
+printk(KERN_INFO "pid %d ret %d",p_id,ret);
+return sizeof(buf);
+}
+
+void create_new_proc_entry()
+{
+proc_write_entry = create_proc_entry(proc_name,0666,NULL);
+if(!proc_write_entry)
+      {
+    printk(KERN_INFO "Error creating proc entry");
+    return -ENOMEM;
+    }
+proc_write_entry->read_proc = read_proc ;
+proc_write_entry->write_proc = write_proc;
+printk(KERN_INFO "proc initialized");
+
+}
+
+
+
+int proc_init (void) {
+    create_new_proc_entry();
+    return 0;
+}
+
+void proc_cleanup(void) {
+    printk(KERN_INFO " Inside cleanup_module\n");
+    remove_proc_entry(proc_name,NULL);
+}
+MODULE_LICENSE("GPL");   
+module_init(proc_init);
+module_exit(proc_cleanup);
+
