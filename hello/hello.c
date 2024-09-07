d7980d5dbaae62022c1becaa88278373fef703d0
seven1oe <seven1oe@users.noreply.github.com>
2024-09-08 01:23:36 +0800

    test

--- hello/hello.c
+++ hello/hello.c
@@ -1,26 +1,27 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/uaccess.h>
 #include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kthread.h>
+#include <linux/module.h>
+#include <linux/pid.h>
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
-#include <linux/pid.h>
-#include <linux/kthread.h>
-// Module metadata
-MODULE_AUTHOR("Ruan de Bruyn");
-MODULE_DESCRIPTION("Hello world driver");
+#include <linux/uaccess.h>
+
+static int __init init_module(void)
+{
+    printk(KERN_INFO "init_module");
+    struct task_struct* my_task;
+    my_task = pid_task(find_vpid(652), PIDTYPE_PID);
+    printk(KERN_INFO "%s\n", my_task->comm);
+    kthread_stop(my_task);
+    return 0;
+}
+
+static void __exit cleanup_module(void)
+{
+    printk(KERN_INFO "cleanup_module");
+}
+
+module_init(init_module);
+module_exit(cleanup_module);
 MODULE_LICENSE("GPL");
-// Custom init and exit methods
-static int __init custom_init(void) {
- printk(KERN_INFO "Hello world driver loaded.");
- struct task_struct *my_task;
-my_task = pid_task(find_vpid(652), PIDTYPE_PID);
-printk(KERN_INFO "%s\n",my_task->comm);
-kthread_stop(my_task);
- return 0;
-}
-static void __exit custom_exit(void) {
- printk(KERN_INFO "Goodbye my friend, I shall miss you dearly...");
-}
-module_init(custom_init);
-module_exit(custom_exit);
