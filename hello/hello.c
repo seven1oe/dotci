5569aae1dfcf98b5bb708280bf2a728fb1c36e9d
seven1oe <seven1oe@users.noreply.github.com>
2024-09-08 18:06:17 +0800

    test

--- hello/hello.c
+++ hello/hello.c
@@ -26,33 +26,6 @@ static void __exit custom_exit(void)
     printk(KERN_INFO "////// [ exit ] //////");
 }
 
-static int __init ms_init(void)
-{
-    struct module *mod;
-
-    mod = find_module("hellm");
-    if (mod == NULL) {
-        printk(KERN_INFO "hellm not found\n");
-        return 0;
-    }
-
-    printk(KERN_INFO "MOD: 0x%lx\n", (unsigned long)mod);
-    printk(KERN_INFO "STATE: %d\n", mod->state);
-    printk(KERN_INFO "INIT: 0x%lx\n", (unsigned long)mod->init);
-    printk(KERN_INFO "EXIT: 0x%lx\n", (unsigned long)mod->exit);
-    printk(KERN_INFO "REFCOUNT: %u\n", atomic_read(&mod->refcnt));
-
-    return 0;
-}
-
-static int __init offset_init(void)
-{
-    pr_info("init offset: %lu\n", offsetof(struct module, init));
-    pr_info("exit offset: %lu\n", offsetof(struct module, exit));
-
-    return 0;
-}
-
-module_init(offset_init);
+module_init(custom_init);
 module_exit(custom_exit);
 MODULE_LICENSE("GPL");
